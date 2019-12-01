b45aec0e8967bb5cd1ba1e272b3cbd6173bc15ff
Baptiste Lagarde <lagarde.baptiste@gmail.com>
2019-12-01 14:34:10 -0500

    Remove extraneous space.

--- arbre/models.py
+++ arbre/models.py
@@ -8,7 +8,7 @@ class Person(models.Model):
     first_name = models.CharField(max_length=20, help_text='First name.')
     middle_name = models.CharField(null=True, max_length=20, help_text='Middle name, space-separated if more than one.')
     last_name = models.CharField(max_length=20, help_text='Last name.')
-    parent =  models.ManyToManyField('Person', related_name='children', help_text='ID of the parent in the Persons table.')
+    parent = models.ManyToManyField('Person', related_name='children', help_text='ID of the parent in the Persons table.')
     birth_place = models.CharField(null=True, max_length=20, help_text='Place of birth, generally a city.')
     birth_date = models.DateField(null=True, help_text='Birth date.')
     birth_date_is_approximative = models.BooleanField(default=False, help_text='Boolean indicating uncertainty around the birth date.')
